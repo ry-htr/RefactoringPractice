6a5d5cb599c8c2d2fa84de605264e813da3b9f00
Ryo Hattori <lagoon1981@gmail.com>
2011-12-08 22:32:11 +0900

    第２章 その２　リファクタリング前

---
+++ src/chapter2/simpledatabase/Main.java
@@ -0,0 +1,23 @@
+package chapter2.simpledatabase;
+
+import java.io.*;
+import java.util.Iterator;
+
+public class Main {
+    public static void main (String[] args) {
+        String sCurrentDir = new File(".").getAbsoluteFile().getParent();
+//        System.out.print(sCurrentDir);
+        try{
+            SimpleDatabase db = new SimpleDatabase(new FileReader(sCurrentDir + "/src/chapter2/simpledatabase/dbfile.txt"));
+            Iterator<String> it = db.iterator();
+            while (it.hasNext()) {
+                String key = it.next();
+                System.out.println("KEY: \"" + key + "\"");
+                System.out.println("VALUE: \"" + db.getValue(key) + "\"");
+                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
