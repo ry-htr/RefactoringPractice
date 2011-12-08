957aba1c9ef8a1495829bcc2974f54db51df78fe
Ryo Hattori <lagoon1981@gmail.com>
2011-12-08 20:50:30 +0900

    第２章　リファクタリング前コミット

---
+++ src/chapter2/FindInt.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+class FindInt {
+    public static boolean find(int[] data, int target) {
+        boolean flag = false;
+        for (int i = 0; i < data.length && !flag; i++) {
+            if (data[i] == target) {
+                flag = true;
+            }
+        }
+        return flag;
+    }
+}
