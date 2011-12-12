a5ab602e6cbdf0a24b4fc523a147ad0f4894b9a7
Ryo Hattori <lagoon1981@gmail.com>
2011-12-12 22:32:35 +0900

    4章リファクタリング前

---
+++ src/chapter4/Label.java
@@ -0,0 +1,17 @@
+package chapter4;
+
+class Label {
+    private final String _label;
+
+    public Label(String label) {
+        _label = label;
+    }
+
+    public void display() {
+        System.out.println("display: " + _label);
+    }
+
+    public String toString() {
+        return "\"" + _label + "\"";
+    }
+}
