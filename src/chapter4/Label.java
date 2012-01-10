cebd55c5c5c633cb3b1feb3b31bbb5562e87f25d
Ryo Hattori <lagoon1981@gmail.com>
2012-01-10 21:16:36 +0900

    4章リファクタリング続き

--- src/chapter4/Label.java
+++ src/chapter4/Label.java
@@ -19,4 +19,25 @@ class Label {
         return false;
     }
 
+    public static Label newNull(){
+        return NullLabel.getInstance();
+    }
+
+    private static class NullLabel extends Label {
+        private static final NullLabel singleton = new NullLabel();
+
+        private static NullLabel getInstance() {
+            return singleton;
+        }
+
+        private NullLabel() {
+            super("(none)");
+        }
+
+        @Override public void display(){}
+
+        @Override public boolean isNull(){
+            return true;
+        }
+    }
 }
