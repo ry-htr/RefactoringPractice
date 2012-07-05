d22ee98d601e08fbbc1aa53bf528d6c8b1b6f17d
neet_orz <lagoon1981@hotmail.com>
2012-07-05 21:30:29 +0900

    chapter10 before refactoring commit

---
+++ test/chapter10/MainTest.java
@@ -0,0 +1,45 @@
+package chapter10;
+
+import junit.framework.JUnit4TestAdapter;
+import org.junit.Test;
+import refbook.StandardOutputTest;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest extends StandardOutputTest {
+    @Test public void testNew() {
+        Robot robot = new Robot("Andrew");
+        String actual = robot.toString();
+        String expected = "[ Robot: Andrew position(0, 0), direction(0, 1) ]";
+        assertEquals(expected, actual);
+    }
+
+    @Test public void testFRF() {
+        Robot robot = new Robot("Andrew");
+        robot.execute("forward right forward");
+        String actual = robot.toString();
+        String expected = "[ Robot: Andrew position(1, 1), direction(1, 0) ]";
+        assertEquals(expected, actual);
+    }
+
+    @Test public void testFRFLBLF() {
+        Robot robot = new Robot("Andrew");
+        robot.execute("forward right forward");
+        robot.execute("left backward left forward");
+        String actual = robot.toString();
+        String expected = "[ Robot: Andrew position(0, 0), direction(-1, 0) ]";
+        assertEquals(expected, actual);
+    }
+
+    @Test public void testError() {
+        Robot robot = new Robot("Andrew");
+        robot.execute("farvard");
+        String actual = getActualOutput();
+        String expected = getExpectedOutput("Invalid command: farvard");
+        assertEquals(expected, actual);
+    }
+
+    public static junit.framework.Test suite() {
+        return new JUnit4TestAdapter(MainTest.class);
+    }
+}
